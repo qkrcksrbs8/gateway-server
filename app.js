5665e4d7a6385a2e3c0431f0187788d4ec84cec2
changyun <qkrcksrbs8@naver.com>
2022-02-27 21:41:29 +0900

    url config 추가 요청 패스에 따른 ip:port 적용

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express'); // npm i express
 const router = express();
-const bodyParser = require("body-parser"); // npm i body-parser
 // api npm // npm i axios
 
+const bodyParser = require("body-parser"); // npm i body-parser
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 //테스트 라우터
 router.get('/test', (req, res) => {
     res.json({'code':'S001'});
+})
+
+//get gateway
+router.get('/*', (req, res) => {
+
+    let startPath = req.path.split('/', 2)[1];
+    let url = require('./config/urlConfig')[startPath].url;
+    if (!url) return res.json({'code':'E001', 'msg':'유효하지 않은 요청입니다.'});
+    url = `${url}${req.path}`;
+    console.log(url);
+
+    res.json({'code':'S001', 'url':url});
 })
 
 //포트 연결
---
+++ config/urlConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    user: {
+        url: 'localhost:3001'
+    },
+    store: {
+        url: 'localhost:3002'
+    }
+}
